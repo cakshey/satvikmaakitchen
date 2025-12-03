28bd4c67417fad1585f0fd3215075e667dfead9d
Akshey Chaubey <66552373+cakshey@users.noreply.github.com>
2025-12-03 18:12:20 +0530

    Add files via upload

---
+++ frontend/app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <head />
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
